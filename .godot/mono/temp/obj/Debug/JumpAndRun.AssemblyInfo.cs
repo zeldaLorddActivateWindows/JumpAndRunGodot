a580c4d2985f2db824a4a86707da8aa956fe0b8c
Josef Lackerbauer <j.lackerbauer@htlvb.at>
2025-06-27 12:22:59 +0200

    implement platformdesign

--- .godot/mono/temp/obj/Debug/JumpAndRun.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/JumpAndRun.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JumpAndRun")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3f44f4ef9ca03ff35a5c891813a51cce0a193c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d71e0daaba178a7be8cb272ac7e09d7c593a90")]
 [assembly: System.Reflection.AssemblyProductAttribute("JumpAndRun")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JumpAndRun")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
